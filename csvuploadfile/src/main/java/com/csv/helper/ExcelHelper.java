17aca378e1984d50ae353a4985357854282dd7ef
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-03-16 19:49:39 +0530

    added csv download functionality

--- csvuploadfile/src/main/java/com/csv/helper/ExcelHelper.java
+++ csvuploadfile/src/main/java/com/csv/helper/ExcelHelper.java
@@ -1,5 +1,8 @@
 package com.csv.helper;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -7,6 +10,8 @@ import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,74 +21,108 @@ import com.csv.entity.File;
 
 public class ExcelHelper {
 
+	public static boolean checkExcelFormat(MultipartFile file) {
 
-    //check that file is of excel type or not
-    public static boolean checkExcelFormat(MultipartFile file) {
+		String contentType = file.getContentType();
 
-        String contentType = file.getContentType();
+		if (contentType.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
+			return true;
+		} else {
+			return false;
+		}
 
-        if (contentType.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
-            return true;
-        } else {
-            return false;
-        }
+	}
+	
 
-    }
+	public static List<CsvEntry> convertExcelToListOfProduct(InputStream is, File f) {
+		List<CsvEntry> list = new ArrayList<>();
 
+		try {
 
-    //convert excel to list of products
+			XSSFWorkbook workbook = new XSSFWorkbook(is);
+			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+				XSSFSheet sheet = workbook.getSheetAt(i);
+				String sheetName = sheet.getSheetName();
+				System.out.println("Sheet name: " + sheetName);
 
-    public static List<CsvEntry> convertExcelToListOfProduct(InputStream is, File f) {
-        List<CsvEntry> list = new ArrayList<>();
+				int rowNumber = 0;
+				Iterator<Row> iterator = sheet.iterator();
 
-        try {
+				while (iterator.hasNext()) {
+					Row row = iterator.next();
 
+					if (rowNumber == 0) {
+						rowNumber++;
+						continue;
+					}
 
-            XSSFWorkbook workbook = new XSSFWorkbook(is);
+					Iterator<Cell> cells = row.iterator();
 
-            XSSFSheet sheet = workbook.getSheet("sheet1");
+					int cid = 0;
 
-            int rowNumber = 0;
-            Iterator<Row> iterator = sheet.iterator();
+					CsvEntry csvEntry = new CsvEntry();
 
-            while (iterator.hasNext()) {
-                Row row = iterator.next();
+					while (cells.hasNext()) {
+						Cell cell = cells.next();
 
-                if (rowNumber == 0) {
-                    rowNumber++;
-                    continue;
-                }
+						switch (cid) {
+						case 0:
+							csvEntry.setFinalColumn(cell.getStringCellValue());
+							break;
+						default:
+							break;
+						}
+						cid++;
+					}
+					csvEntry.setFile(f);
+					list.add(csvEntry);
 
-                Iterator<Cell> cells = row.iterator();
+				}
 
-                int cid = 0;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
 
-                CsvEntry csvEntry = new CsvEntry();
+	}
 
-                while (cells.hasNext()) {
-                    Cell cell = cells.next();
+	public static String[] HEADERS = { "finalColumn" };
 
-                    switch (cid) {
-                        case 0:
-                        	csvEntry.setFinalColumn(cell.getStringCellValue());
-                            break;
-                        default:
-                            break;
-                    }
-                    cid++;
-                }
-                csvEntry.setFile(f);
-                list.add(csvEntry);
+	public static String SHEET_NAME = "csv_upload";
 
-            }
+	public static ByteArrayInputStream dataToExcel(List<CsvEntry> list) {
+		try {
 
+			Workbook workbook = new XSSFWorkbook();
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return list;
+			Sheet sheet = workbook.createSheet(SHEET_NAME);
 
-    }
+			Row row = sheet.createRow(0);
 
+			for (int i = 0; i < HEADERS.length; i++) {
+				Cell cell = row.createCell(i);
+				cell.setCellValue(HEADERS[i]);
+			}
+
+			int rowIndex = 1;
+
+			for (CsvEntry c : list) {
+				Row dataRow = sheet.createRow(rowIndex);
+				rowIndex++;
+
+				dataRow.createCell(0).setCellValue(c.getFinalColumn());
+			}
+
+			workbook.write(out);
+
+			return new ByteArrayInputStream(out.toByteArray());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
--- csvuploadfile/src/main/java/com/csv/repository/CsvEntryRepository.java
+++ csvuploadfile/src/main/java/com/csv/repository/CsvEntryRepository.java
@@ -1,9 +1,14 @@
 package com.csv.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.csv.entity.CsvEntry;
+import com.csv.entity.File;
 
-public interface CsvEntryRepository extends JpaRepository<CsvEntry, String>{
+public interface CsvEntryRepository extends JpaRepository<CsvEntry, Long>{
+
+	List<CsvEntry> findByFile(File f);
 
 }
